97a10a63061fe688191a86441099a391668f88e3
magynhard <github@mail.magynhard.de>
2024-06-26 13:13:23 +0200

    add event test

--- spec/app/core/sipa-component-spec.js
+++ spec/app/core/sipa-component-spec.js
@@ -67,16 +67,40 @@ describe('SipaComponent', () => {
         });
         it('"before_update" event', function () {
             const compo = new SpecOneComponent();
+            let before_val = null;
             const fun = (component, data, options) => {
                 data ??= {};
                 data.mychild ??= {};
                 data.mychild.family = "Speculatius";
+                before_val = component._data.attr;
             }
             compo.initTemplate();
             expect(compo.data().mychild.family).toEqual("Chuck");
             compo.events().subscribe("before_update", fun);
             compo.update({ attr: true });
+            expect(before_val).toEqual(undefined);
+            compo.update({ attr: false });
+            expect(before_val).toEqual(true);
             expect(compo.data().mychild.family).toEqual("Speculatius");
+            expect(compo.data().attr).toEqual(false);
+        });
+        it('"after_update" event', function () {
+            const compo = new SpecOneComponent();
+            let after_val = null;
+            const fun = (component, data, options) => {
+                data ??= {};
+                data.mychild ??= {};
+                data.mychild.family = "Bruno";
+                after_val = component._data.attr;
+            }
+            compo.initTemplate();
+            expect(compo.data().mychild.family).toEqual("Chuck");
+            compo.events().subscribe("after_update", fun);
+            expect(after_val).toEqual(null);
+            compo.update({ attr: true });
+            expect(after_val).toEqual(true);
+            // data is not used or assigned on after_update
+            expect(compo.data().mychild.family).toEqual("Chuck");
         });
     });
 });
