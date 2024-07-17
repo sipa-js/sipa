b0196353fede9c8d09027b6deb0beebcfff58b66
magynhard <github@mail.magynhard.de>
2024-07-17 14:58:05 +0200

    add nested slots test

--- spec/app/core/sipa-component-slots-instance-spec.js
+++ spec/app/core/sipa-component-slots-instance-spec.js
@@ -9,9 +9,12 @@ class SlotComponent extends SipaComponent {
 }
 class SlotNestComponent extends SipaComponent {
 }
+class SlotSuperNestComponent extends SipaComponent {
+}
 
 SipaComponent.registerComponent(SlotComponent);
 SipaComponent.registerComponent(SlotNestComponent);
+SipaComponent.registerComponent(SlotSuperNestComponent);
 
 
 //----------------------------------------------------------------------------------------------------
@@ -49,10 +52,36 @@ describe('SipaComponent', () => {
             const comp = new SlotComponent({},{
                 content: `TopLevel<div slot="header"><slot-nest-component sipa-alias="nestos"><slot>Boratza</slot></slot-nest-component></div>`
             });
+            const match = `<slot-component sipa-id="[0-9]+">Parentus<div slot="header"><slot-nest-component sipa-id="[0-9]+">NestedBoratza</slot-nest-component></div>TopLevelPinguin</slot-component>`;
             // first init
-            expect(comp.html()).toMatch(`<slot-component sipa-id="[0-9]+">Parentus<div slot="header"><slot-nest-component sipa-id="[0-9]+">NestedBoratza</slot-nest-component></div>TopLevelPinguin</slot-component>`);
-            // second init (caching working?)
-            expect(comp.html()).toMatch(`<slot-component sipa-id="[0-9]+">Parentus<div slot="header"><slot-nest-component sipa-id="[0-9]+">NestedBoratza</slot-nest-component></div>TopLevelPinguin</slot-component>`);
+            expect(comp.html()).toMatch(match);
+            // second init (check if caching is working properly)
+            expect(comp.html()).toMatch(match);
+        });
+        it('can nest components in slots twice', function () {
+            class SlotOneComponent extends SipaComponent {
+                static template = () => {
+                    return `<slot-one-component><slot></slot>Slot</slot-one-component>`;
+                }
+            }
+            class SlotTwoComponent extends SipaComponent {
+                static template = () => {
+                    return `<slot-two-component><slot></slot>Nest</slot-two-component>`;
+                }
+            }
+            class SlotThreeComponent extends SipaComponent {
+                static template = () => {
+                    return `<slot-three-component><slot></slot>Super</slot-three-component>`;
+                }
+            }
+            const comp = new SlotOneComponent({},{
+                content: `First<slot-two-component sipa-alias="nest">Second<slot-three-component sipa-alias="super">Third</slot-three-component></slot-two-component></div>`
+            });
+            // first init
+            const match = `<slot-one-component sipa-id="[0-9]+">First<slot-two-component sipa-id="[0-9]+">Second<slot-three-component sipa-id="[0-9]+">ThirdSuper</slot-three-component>Nest</slot-two-component>Slot</slot-one-component>`;
+            expect(comp.html()).toMatch(match);
+            // second init (check if caching is working properly)
+            expect(comp.html()).toMatch(match);
         });
     });
 });
