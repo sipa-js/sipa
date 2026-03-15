5f0c9152904f19eb160f3c8abf7c744445c6a28a
magynhard <github@mail.magynhard.de>
2026-03-15 21:50:21 +0100

    implement poc of checkbox

--- src/playground/assets/elements/sipa-checkbox/sipa-checkbox.js
+++ src/playground/assets/elements/sipa-checkbox/sipa-checkbox.js
@@ -1,20 +1,61 @@
 
 class SipaCheckbox extends SipaElement {
 
-    constructor() {
-        super();
+    static get observedAttributes() {
+        return ['checked'];
     }
 
-    onClick(e) {
-        if(e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
-            this.setState({ checked: e.target.checked }, true);
+    constructor() {
+        super();
+        this._onHostClick = this._onHostClick.bind(this);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.removeEventListener('click', this._onHostClick);
+        this.addEventListener('click', this._onHostClick);
+        this.setChecked(this.hasAttribute('checked'), false);
+    }
+
+    disconnectedCallback() {
+        this.removeEventListener('click', this._onHostClick);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if(name === 'checked' && oldValue !== newValue) {
+            this.setState({ checked: newValue !== null }, false);
         }
     }
 
     onInternalChange(e) {
         if(e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
-            this.setState({ checked: e.target.checked }, true);
+            this.setChecked(e.target.checked, true);
         }
+    }
+
+    _onHostClick(e) {
+        if(e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
+            return;
+        }
+
+        this.toggleChecked();
+    }
+
+    setChecked(checked, shouldBubble = true) {
+        this.toggleAttribute('checked', checked);
+
+        if(this.state.checked !== checked) {
+            this.setState({ checked }, false);
+        }
+
+        if(shouldBubble) {
+            this._emitChange();
+        }
+    }
+
+    toggleChecked() {
+        this.setChecked(!this.hasAttribute('checked'), true);
     }
 
     template() {
